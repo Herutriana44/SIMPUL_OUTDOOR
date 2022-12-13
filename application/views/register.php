594949e7de70a14c14b3fe172d3c54db0e6d5a54
ainurropiq <ainurropiq09@gmail.com>
2022-12-13 10:56:51 +0800

    ropiq : pembaruan views

--- application/views/register.php
+++ application/views/register.php
@@ -5,19 +5,19 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Form pendaftaran simpul Outdoor</title>
-    <link rel="stylesheet" href="<?php echo asset_url();?>Register-css.css">
+    <link rel="stylesheet" href="<?php echo asset_url();?>register.css">
 </head>
 <body>
     <!-- Ini Form Untuk Registrasi -->
     <div class="register-card">
         <h2>Form Pendaftaran Simpul Outdoor</h2>
-        <form class="register-form" action="<?php echo base_url('Register/registration'); ?>" method="post">
+        <form class="register-form" method="post">
             <input type="text" placeholder="Nama Lengkap" id="fullname" name="fullname"/>
             <input type="text" placeholder="Alamat Lengkap" id="alamat" name="alamat"/>
             <input type="text" placeholder="Nomor Telepon" id="notlp" name="notlp"/>
             <input type="text" placeholder="Email" id="username" name="username"/>
             <input type="password" placeholder="Password" id="password" name="password"/>
-            <input type="submit" value="Daftar">
+            <input class="button" type="submit" value="Daftar">
         </form>
 </body>
 </html>
